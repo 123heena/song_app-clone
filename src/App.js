6ce80bd247d4fc5be1ed834a77846866d03714d1
123zoya <heenak786.23@gmail.com>
2023-11-27 15:32:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import HeaderBar from "./HeaderBar";
+import Footer from "./Footer";
+const myStyle={
+  backgroundImage: "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.in%2FFirst-Traditional-Coaster-Painting-Decoration%2Fdp%2FB0BYJ2ZVC3&psig=AOvVaw1adZdtRntjrIouNVgLfa3g&ust=1701155230038000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJj_zqLP44IDFQAAAAAdAAAAABAI')",
+  height:'100vh',
+  marginTop:'-70px',
+  fontSize:'50px',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+};
+function App()
+{
+  return(
+    <div style={myStyle}>
+      <HeaderBar/>
+      <Footer/>
+      </div>
+  )
 }
-
 export default App;
